28d1ce06be9e4be432789f5cf7743ba8fee17ffd
OniDem1 <spaindecraft17@gmail.com>
2023-06-23 11:35:29 +0700

    Добавьте файлы проекта.

---
+++ Legendre/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Program
+{
+    class Legendre
+    {
+        static void Legendr(int p, int n)
+        {
+            int sum = 0;
+            for (int i = 1; i < n;i++)
+            {
+                
+                double stage = Math.Pow(Convert.ToDouble(p), Convert.ToDouble(i));
+                int nums = n / Convert.ToInt32(stage);
+                if (stage < nums)
+                {
+                    sum += nums;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        public static int Main()
+        {
+            Console.WriteLine("Введите p");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (p < 0 || n < 0)
+            {
+                Console.WriteLine("Числа отризательные");
+                return 0;
+            }
+            else if (p > n)
+            {
+                Console.WriteLine("Результат 0");
+                return 0;
+            }
+            Legendr(p, n);
+            return 0;
+        }
+    }
+}
